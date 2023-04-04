1b90351e3d769bbaefbc930cf3c6a9a8ecec6d4b
snide02 <michaelsnider2002@gmail.com>
2023-04-04 12:43:38 -0400

    Rocket moves straight,  LaserGun code removed.
    Rocket variable name was changed from Rock to Rocket for clarity.
    The Rocket currently uses thrust just to move itself forward until it hits the border of the world.
    
    Code that calculates the Laser's angle and the debug lines have been removed, as well as the variables used by the Laser Gun.

--- static/game.js
+++ static/game.js
@@ -4,26 +4,16 @@ import {
 import Lobby from "./Lobby.js"
 
 var pointer; //variable for mouse's location
-var line1;
-var graphics;
-var laserLength; //length of the Laser
-var laserX; //X coordinate for the end of the laser
-var laserY; //Y coordinate for the end of the laser
 var gun; //laser gun
 var input; //mouse position for sprites
-var point;
-var graphics
-var Slope;
-var CheckY;
-var CheckB;
+
 //Rocket Launcher Vars
-var rocket; // rocket launcher
-var rock; // rocket 
-var rockX; // rocket X
-var rockY; // rocket Y 
+var rocket; // rocket that gets fired
 
 // Exampl Missle follow mouse
 // https://blog.ourcade.co/posts/2020/make-homing-missile-seek-target-arcade-physics-phaser-3/
+
+var launched = false;
 
 class gameScene extends Phaser.Scene {
 
@@ -38,8 +28,8 @@ class gameScene extends Phaser.Scene {
   //image preloads for car and gun
   preload() {
     this.load.image('car', 'static/assets/car.png')
-    this.load.image('gun', 'static/assets/rocket_trans.png')
-    this.load.image('rock', 'static/assets/rocket_single.png')
+    this.load.image('gun', 'static/assets/rocketLauncher.png')
+    this.load.image('rocket', 'static/assets/rocket_trans.png')
   }
 
   create() {
@@ -58,13 +48,13 @@ class gameScene extends Phaser.Scene {
     //sends the enetered player name of this client to server so that it can be stored in array
     self.socket.emit('updateName', self.playerName)
 
-    // //adds gun sprite-image
+    //adds gun sprite-image
     gun = this.add.sprite(400, 300, 'gun');
     gun.setDepth(1);
 
     //adds rocket sprite-image
-    rocket = this.add.sprite(400, 300, 'rock');
-    rocket.setDepth(2);
+    rocket = this.add.sprite(400, 300, 'rocket');
+    //rocket.setDepth(2);
 
     //array to store other players
     this.otherPlayers = this.add.group()
@@ -122,12 +112,6 @@ class gameScene extends Phaser.Scene {
       })
     })
 
-
-    // graphics = this.add.graphics({ fillStyle: { color: 0x2266aa } });
-
-    // point = new Phaser.Geom.Point(300, 575);
-    // graphics.fillPointShape(point, 10);
-
   }
 
 
@@ -137,53 +121,21 @@ class gameScene extends Phaser.Scene {
     //sets rotation of laser gun
     let angle = Phaser.Math.Angle.Between(gun.x, gun.y, input.x, input.y);
     gun.setRotation(angle);
-    // let rockangle = Phaser.Math.Angle.car.Angle;
-    // console.log(rockangle);
-    // console.log(car.Angle);
 
     //Make sure car has been instantiated correctly
     if (this.car) {
 
-      if (line1)
-        graphics.destroy(line1); //deletes the line, so that they don't build up
       pointer = this.input.activePointer; //sets pointer to user's mouse
-      laserLength = Math.sqrt((pointer.worldY - this.car.y) ** 2 + (pointer.worldX - this.car.x) ** 2);
-      laserY = laserLength * (pointer.worldY - this.car.y);
-      laserX = laserLength * (pointer.worldX - this.car.x);
-      line1 = new Phaser.Geom.Line(this.car.x, this.car.y, laserX, laserY);
-      graphics = this.add.graphics({
-        lineStyle: {
-          width: 4,
-          color: 0xaa00aa
-        }
-      });
-      //graphics.strokeLineShape(line1); //draws the line
       gun.x = this.car.x;
       gun.y = this.car.y;
       this.car.gunrotation = gun.rotation;
 
-      // rocket.x = this.car.x;
-      // rocket.y = this.car.y;
-
-      // Slope = ((pointer.worldY - this.car.y) / (pointer.worldX - this.car.x));
-      // CheckB = this.car.y - (Slope * this.car.x)
-      // CheckY = ((Slope * point.x) + CheckB);
-
-      // // Collision detection
-      // const collisionThreshold = 25;
-      // if (Math.abs(CheckY - point.y) < collisionThreshold) {
-      //   console.log("Collision detected");
-      // }
-
-
-      // if (CheckY < point.y) {
-      //   console.log("Laser above dot")
-      // }
-
-      // if (CheckY > point.y) {
-      //   console.log("Laser below dot")
-      // }
-
+      //rocket.x = this.car.x;
+      if(this.input.activePointer.isDown && launched == false){
+          rocket = this.matter.add.gameObject(rocket);
+          rocket.thrust(0.2);
+          launched = true;
+      }
 
       //Drive according to logic in player object
       //function takes: car object, label object, input system, time delta, and socket object
@@ -196,12 +148,10 @@ class gameScene extends Phaser.Scene {
       //Check health every frame (do not delete)
       Player.updateHealth(this.car, this.socket);
 
+
     }
 
-    // console.log(CheckY)
-    // console.log(point.y)
-    // console.log(point.x)
-
+     
   }
 
 }
